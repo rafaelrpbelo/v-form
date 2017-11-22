462e10fb371582c497dd2b25f313afae81ea0ea5
Rafael Belo <rafaelrpbelo@gmail.com>
2017-11-21 23:31:19 -0300

    Create intruction in root component

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,18 @@
 <template>
-  <v-form></v-form>
+  <div class='container'>
+    <div class='row'>
+      <div class='col'>
+        <div class="jumbotron">
+          <h1 class="display-4">Welcome to V-Form!</h1>
+          <p class="lead">This is my quick reference about dynamic forms using Vue.js.</p>
+
+          <hr class="my-4">
+
+          <v-form></v-form>
+        </div>
+      </div>
+    </div>
+  </div>
 </template>
 
 <script>
